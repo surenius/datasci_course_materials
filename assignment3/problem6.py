0113ce3c836dad303fba74a2e2e10a6ea5fa2003
surenius <suren.aladjiev@gmail.com>
2015-11-25 20:05:16 -0800

    ass3: 50/60, passed, one problem left

--- assignment3/problem6.py
+++ assignment3/problem6.py
@@ -9,6 +9,8 @@ mr = MapReduce.MapReduce()
 # =============================
 # Do not modify above this line
 
+n = 5
+
 def mapper(record):
     # record comes as: [a|b, i, j, value]
     matrix = record[0]
@@ -16,28 +18,32 @@ def mapper(record):
     j = record[2]
     value = record[3]
     if (matrix == "a"):
-      for k in range(j):
-        mr.emit_intermediate((i, k), (matrix, 0))
-      mr.emit_intermediate((i, j), (matrix, value))
+      for k in range(n):
+        mr.emit_intermediate((i, k), (matrix, j, value))
     else:
-      for k in range(i):
-        mr.emit_intermediate((j, k), (matrix, 0))
-      mr.emit_intermediate((j, i), (matrix, value))
+      for k in range(n):
+        mr.emit_intermediate((k, j), (matrix, i, value))
       
 
 def reducer(key, list_of_values):
     # key: (i, j)
     # value: list of (matrix, value) tuples
     
-    d = {"a":[], "b":[]}
-    for (matrix, value) in list_of_values:
-      d[matrix].append(value)
-    
+    a = {}
+    b = {}
+    for (matrix, j, value) in list_of_values:
+      if (matrix == "a"):
+        a[j] = value
+      else:
+        b[j] = value
+        
     s = 0 
-    for (lv, rv) in zip(d.values()[0], d.values()[1]):
-      s = s + lv * rv
+    for j in range(n):
+      lv = a[j] if j in a else 0
+      rv = b[j] if j in b else 0
+      s += lv * rv
       
-    mr.emit((key, s))
+    mr.emit((key[0], key[1], s))
 
 # Do not modify below this line
 # =============================
